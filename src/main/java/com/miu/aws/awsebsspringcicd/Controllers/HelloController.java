31a5811b5c9548864602344afa7ef88b42c61f48
brhanebt <33482700+brhanebt@users.noreply.github.com>
2020-04-05 12:17:13 -0500

    added get mapping

--- src/main/java/com/miu/aws/awsebsspringcicd/Controllers/HelloController.java
+++ src/main/java/com/miu/aws/awsebsspringcicd/Controllers/HelloController.java
@@ -1,11 +1,13 @@
 package com.miu.aws.awsebsspringcicd.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
+    @GetMapping
     public String Hello(){
         System.out.println("hello");
         return "Hello Controller";
